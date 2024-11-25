1a038477abf57d940ad3e265a099fb7b792f9edb
traviscreates <traviscreates@proton.me>
2024-11-24 23:50:36 -0800

    extended expression parsing

--- ast/ast.cpp
+++ ast/ast.cpp
@@ -1,4 +1,7 @@
+#include <cstdint>
+#include <sstream>
 #include "ast.h"
+#include "token.h"
 
 namespace ast {
 
@@ -16,10 +19,28 @@ std::string LetStatement::TokenLiteral() const {
     return Token.Literal;
 }
 
+std::string LetStatement::String() const {
+    std::string result = TokenLiteral() + " " + Name->String() + " = ";
+    if (Value) {
+        result += Value->String();
+    }
+    result += ";";
+    return result;
+}
+
 void ReturnStatement::statementNode() { }
 
 std::string ReturnStatement::TokenLiteral() const {
     return Token.Literal;
+}
+
+std::string ReturnStatement::String() const {
+    std::string result = TokenLiteral() + " ";
+    if (ReturnValue) {
+        result += ReturnValue->String();
+    }
+    result += ";";
+    return result;
 }
 
 void Identifier::expressionNode() { }
@@ -33,18 +54,89 @@ std::string Identifier::TokenLiteral() const {
     return Token.Literal;
 }
 
+std::string Identifier::String() const {
+    return Value;
+}
+
 std::string Program::String() const {
     std::string result;
     for (const auto& stmt : Statements) {
-        result += stmt->TokenLiteral() + " ";
-        auto letStmt = dynamic_cast<const LetStatement*>(stmt.get());
-        if (letStmt) {
-            result += letStmt->Name->TokenLiteral() + " = " + letStmt->Value->TokenLiteral() + ";";
-        }
+        result += stmt->String();
     }
     return result;
 }
 
 void ExpressionStatement::statementNode() { }
+
+std::string ExpressionStatement::TokenLiteral() const {
+    return Token.Literal;
+}
+
+std::string ExpressionStatement::String() const {
+    if (Expression) {
+        return Expression->String();
+    }
+    return "";
+}
+
+IntegerLiteral::IntegerLiteral(const token::Token& tok)
+    : Token(tok) { }
+
+IntegerLiteral::IntegerLiteral(const token::Token& tok, const int64_t& val)
+    : Token(tok), Value(val) { }
+
+void IntegerLiteral::expressionNode() { }
+
+std::string IntegerLiteral::TokenLiteral() const {
+    return Token.Literal;
+}
+
+std::string IntegerLiteral::String() const {
+    return Token.Literal;
+}
+
+PrefixExpression::PrefixExpression(const token::Token& tok, const std::string& op)
+    : Token(tok), Operator(op) { }
+
+void PrefixExpression::expressionNode() { }
+
+std::string PrefixExpression::TokenLiteral() const {
+    return Token.Literal;
+}
+
+std::string PrefixExpression::String() const {
+    std::ostringstream out;
+    out << "(" << Operator;
+    if (Right) {
+        out << Right->String();
+    }
+    out << ")";
+    return out.str();
+}
+
+InfixExpression::InfixExpression(const token::Token& tok, std::unique_ptr<Expression> left, const std::string& op, std::unique_ptr<Expression> right)
+    : Token(tok), Left(std::move(left)), Operator(op), Right(std::move(right)) { }
+
+void InfixExpression::expressionNode() { }
+
+std::string InfixExpression::TokenLiteral() const {
+    return Token.Literal;
+}
+
+std::string InfixExpression::String() const {
+    std::ostringstream out;
+    out << "(" << Left->String() << " " << Operator << " " << Right->String() << ")";
+    return out.str();
+}
+
+void Boolean::expressionNode() { }
+
+std::string Boolean::TokenLiteral() const {
+    return Token.Literal;
+}
+
+std::string Boolean::String() const {
+    return Token.Literal;
+}
 
 }
--- parser/parser_test.cpp
+++ parser/parser_test.cpp
@@ -49,16 +49,111 @@ bool testReturnStatement(ast::Statement* s) {
     return true;
 }
 
+bool testBoolean(const ast::Expression* expr, bool value) {
+    const auto* boolean = dynamic_cast<const ast::Boolean*>(expr);
+    if (boolean->Value != value) {
+        ADD_FAILURE() << "Boolean value is not " << value << ". Got: " << boolean->Value;
+        return false;
+    }
+    return true;
+}
+
+bool testIntegerLiteral(const ast::Expression* expr, int64_t value) {
+    const auto* integerLiteral = dynamic_cast<const ast::IntegerLiteral*>(expr);
+    if (!integerLiteral) {
+        ADD_FAILURE() << "Expression is not ast::IntegerLiteral. Got: " << typeid(*expr).name();
+        return false;
+    }
+    if (integerLiteral->Value != value) {
+        ADD_FAILURE() << "IntegerLiteral value is not " << value << ". Got: " << integerLiteral->Value;
+        return false;
+    }
+    return true;
+}
+
+bool testIdentifier(const ast::Expression* expr, std::string value) {
+    const auto* identifier = dynamic_cast<const ast::Identifier*>(expr);
+    if (!identifier) {
+        ADD_FAILURE() << "Expression is not ast::Identifier. Got: " << typeid(*expr).name();
+        return false;
+    }
+    if (identifier->Value != value) {
+        ADD_FAILURE() << "Identifier value is not " << value << ". Got: " << identifier->Value;
+        return false;
+    }
+    if (identifier->TokenLiteral() != value) {
+        ADD_FAILURE() << "Identifier TokenLiteral is not " << value << ". Got: " << identifier->TokenLiteral();
+        return false;
+    }
+    return true;
+}
+
+using LiteralExpectedType = std::variant<int64_t, std::string>;
+
+bool testLiteralExpression(const ast::Expression& exp, const LiteralExpectedType& expected) {
+    return std::visit([&exp](auto&& arg) -> bool {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, bool>) {
+            return testBoolean(&exp, arg);
+        } else if constexpr (std::is_same_v<T, int64_t>) {
+            return testIntegerLiteral(&exp, arg);
+        } else if constexpr (std::is_same_v<T, std::string>) {
+            return testIdentifier(&exp, arg);
+        } else {
+            std::cerr << "Type of exp not handled." << std::endl;
+            return false;
+        }
+    }, expected);
+}
+
+// TODO: refactor tests
+bool testInfixExpression(
+    const ast::Expression* expr,
+    const std::variant<bool, int64_t, std::string>& left,
+    const std::string& operator_,
+    const std::variant<bool, int64_t, std::string>& right
+) {
+    const auto* opExp = dynamic_cast<const ast::InfixExpression*>(expr);
+    if (!opExp) {
+        ADD_FAILURE() << "Expression is not ast::InfixExpression. Got: "
+                      << typeid(*expr).name();
+        return false;
+    }
+
+    auto testLeft = [&](const auto& val) -> bool {
+        return testLiteralExpression(*opExp->Left.get(), val);
+    };
+    if (!std::visit(testLeft, left)) {
+        return false;
+    }
+
+    if (opExp->Operator != operator_) {
+        ADD_FAILURE() << "Operator is not '" << operator_ << "'. Got: "
+                      << opExp->Operator;
+        return false;
+    }
+
+    auto testRight = [&](const auto& val) -> bool {
+        return testLiteralExpression(*opExp->Right.get(), val);
+    };
+    if (!std::visit(testRight, right)) {
+        return false;
+    }
+
+    return true;
+}
+
 TEST(ParserTests, TestLetStatements) {
     std::string input = R"(
 let x = 5;
 let y = 10;
 let foobar = 838383;
 )";
-
     auto l = std::make_unique<lexer::Lexer>(input);
     auto p = parser::New(std::move(l));
+
     std::unique_ptr<ast::Program> program = p->ParseProgram();
+
     checkParserErrors(*p);
     ASSERT_NE(program, nullptr);
     ASSERT_EQ(program->Statements.size(), 3);
@@ -68,7 +163,6 @@ let foobar = 838383;
     };
 
     std::vector<Test> tests = { {"x"}, {"y"}, {"foobar"} };
-
     for (size_t i = 0; i < tests.size(); ++i) {
         auto stmt = program->Statements[i].get();
         EXPECT_TRUE(testLetStatement(stmt, tests[i].expectedIdentifier));
@@ -81,10 +175,11 @@ return 5;
 return 10;
 return 993322;
 )";
-
     auto l = std::make_unique<lexer::Lexer>(input);
     auto p = parser::New(std::move(l));
+
     std::unique_ptr<ast::Program> program = p->ParseProgram();
+
     checkParserErrors(*p);
     ASSERT_NE(program, nullptr);
     ASSERT_EQ(program->Statements.size(), 3);
@@ -94,10 +189,271 @@ return 993322;
     };
 
     std::vector<Test> tests = { {"5"}, {"10"}, {"993322"} };
-
     for (size_t i = 0; i < tests.size(); ++i) {
         auto stmt = program->Statements[i].get();
         EXPECT_TRUE(testReturnStatement(stmt));
     }
 }
 
+TEST(ParserTests, TestIdentifierExpression) {
+    std::string input = "foobar;";
+    auto lexer = std::make_unique<lexer::Lexer>(input);
+    auto parser = parser::New(std::move(lexer));
+
+    auto program = parser->ParseProgram();
+
+    checkParserErrors(*parser);
+    ASSERT_EQ(program->Statements.size(), 1) << "Program has incorrect number of statements.";
+
+    auto stmt = dynamic_cast<ast::ExpressionStatement*>(program->Statements[0].get());
+    ASSERT_NE(stmt, nullptr) << "First statement is not an ExpressionStatement.";
+
+    auto ident = dynamic_cast<ast::Identifier*>(stmt->Expression.get());
+    ASSERT_NE(ident, nullptr) << "Expression is not an Identifier.";
+    EXPECT_EQ(ident->Value, "foobar") << "Identifier value is incorrect.";
+    EXPECT_EQ(ident->TokenLiteral(), "foobar") << "Identifier TokenLiteral is incorrect.";
+}
+
+TEST(ParserTests, TestIntegerLiteralExpression) {
+    std::string input = "5;";
+    auto lexer = std::make_unique<lexer::Lexer>(input);
+    auto parser = parser::New(std::move(lexer));
+
+    auto program = parser->ParseProgram();
+
+    checkParserErrors(*parser);
+    ASSERT_EQ(program->Statements.size(), 1) << "Program has incorrect number of statements.";
+
+    auto stmt = dynamic_cast<ast::ExpressionStatement*>(program->Statements[0].get());
+    ASSERT_NE(stmt, nullptr) << "First statement is not an ExpressionStatement.";
+
+    auto int_lit = dynamic_cast<ast::IntegerLiteral*>(stmt->Expression.get());
+    ASSERT_NE(int_lit, nullptr) << "Expression is not an Identifier.";
+    EXPECT_EQ(int_lit->Value, 5) << "Identifier value is incorrect.";
+    EXPECT_EQ(int_lit->TokenLiteral(), "5") << "Identifier TokenLiteral is incorrect.";
+}
+
+// TODO: refactor to combine int64_t and bool types
+TEST(ParserTests, TestParsingPrefixExpressions) {
+    struct PrefixTest {
+        std::string input;
+        std::string operator_;
+        int64_t integerValue;
+    };
+
+    std::vector<PrefixTest> prefixTests = {
+        {"!5;", "!", 5},
+        {"-15;", "-", 15},
+    };
+
+    for (const auto& tt : prefixTests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = std::make_unique<parser::Parser>(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_EQ(program->Statements.size(), 1) << "program.Statements does not contain 1 statement";
+
+        const auto* stmt = dynamic_cast<const ast::ExpressionStatement*>(program->Statements[0].get());
+        ASSERT_NE(stmt, nullptr) << "program.Statements[0] is not ast::ExpressionStatement";
+
+        const auto* exp = dynamic_cast<const ast::PrefixExpression*>(stmt->Expression.get());
+        ASSERT_NE(exp, nullptr) << "stmt is not ast::PrefixExpression";
+        EXPECT_EQ(exp->Operator, tt.operator_) << "exp.Operator is not '" << tt.operator_ << "'";
+        EXPECT_TRUE(testIntegerLiteral(exp->Right.get(), tt.integerValue));
+    }
+}
+
+// TODO: refactor to combine int64_t and bool types
+TEST(ParserTests, TestParsingPrefixExpressionsWithBooleanInput) {
+    struct PrefixTest {
+        std::string input;
+        std::string operator_;
+        bool value;
+    };
+
+    std::vector<PrefixTest> prefixTests = {
+        {"!true;", "!", true},
+        {"!false;", "!", false}
+    };
+
+    for (const auto& tt : prefixTests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = std::make_unique<parser::Parser>(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_EQ(program->Statements.size(), 1) << "program.Statements does not contain 1 statement";
+
+        const auto* stmt = dynamic_cast<const ast::ExpressionStatement*>(program->Statements[0].get());
+        ASSERT_NE(stmt, nullptr) << "program.Statements[0] is not ast::ExpressionStatement";
+
+        const auto* exp = dynamic_cast<const ast::PrefixExpression*>(stmt->Expression.get());
+        ASSERT_NE(exp, nullptr) << "stmt is not ast::PrefixExpression";
+        EXPECT_EQ(exp->Operator, tt.operator_) << "exp.Operator is not '" << tt.operator_ << "'";
+        EXPECT_TRUE(testBoolean(exp->Right.get(), tt.value));
+    }
+}
+
+// TODO: use testInfixExpression
+TEST(ParserTests, TestParsingInfixExpressions) {
+    struct InfixTest {
+        std::string input;
+        int64_t leftValue;
+        std::string op;
+        int64_t rightValue;
+    };
+
+    std::vector<InfixTest> infixTests = {
+        {"5 + 5;", 5, "+", 5},
+        {"5 - 5;", 5, "-", 5},
+        {"5 * 5;", 5, "*", 5},
+        {"5 / 5;", 5, "/", 5},
+        {"5 > 5;", 5, ">", 5},
+        {"5 < 5;", 5, "<", 5},
+        {"5 == 5;", 5, "==", 5},
+        {"5 != 5;", 5, "!=", 5}
+    };
+
+    for (const auto& tt : infixTests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = parser::New(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_EQ(program->Statements.size(), 1) << "program.Statements does not contain 1 statement";
+
+        auto stmt = dynamic_cast<ast::ExpressionStatement*>(program->Statements[0].get());
+        ASSERT_NE(stmt, nullptr) << "program.Statements[0] is not ast::ExpressionStatement";
+
+        auto infixExpr = dynamic_cast<ast::InfixExpression*>(stmt->Expression.get());
+        ASSERT_NE(infixExpr, nullptr) << "stmt.Expression is not ast::InfixExpression";
+        EXPECT_TRUE(testIntegerLiteral(infixExpr->Left.get(), tt.leftValue));
+        EXPECT_EQ(infixExpr->Operator, tt.op) << "exp.Operator is not '" << tt.op << "'";
+        EXPECT_TRUE(testIntegerLiteral(infixExpr->Right.get(), tt.rightValue));
+    }
+}
+
+// TODO: use testInfixExpression
+TEST(ParserTests, TestParsingInfixBooleanExpressions) {
+    struct InfixTest {
+        std::string input;
+        bool leftValue;
+        std::string op;
+        bool rightValue;
+    };
+
+    std::vector<InfixTest> infixTests = {
+        {"true == true", true, "==", true},
+        {"true != false", true, "!=", false},
+        {"false == false", false, "==", false}
+    };
+
+    for (const auto& tt : infixTests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = parser::New(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_EQ(program->Statements.size(), 1) << "program.Statements does not contain 1 statement";
+
+        auto stmt = dynamic_cast<ast::ExpressionStatement*>(program->Statements[0].get());
+        ASSERT_NE(stmt, nullptr) << "program.Statements[0] is not ast::ExpressionStatement";
+
+        auto infixExpr = dynamic_cast<ast::InfixExpression*>(stmt->Expression.get());
+        ASSERT_NE(infixExpr, nullptr) << "stmt.Expression is not ast::InfixExpression";
+        EXPECT_TRUE(testBoolean(infixExpr->Left.get(), tt.leftValue));
+        EXPECT_EQ(infixExpr->Operator, tt.op) << "exp.Operator is not '" << tt.op << "'";
+        EXPECT_TRUE(testBoolean(infixExpr->Right.get(), tt.rightValue));
+    }
+}
+
+TEST(ParserTests, TestParsingOperatorPrecedence) {
+    struct OperatorPrecedenceTest {
+        std::string input;
+        std::string expected;
+    };
+
+    std::vector<OperatorPrecedenceTest> tests {
+        { "-a * b", "((-a) * b)" },
+        { "!-a", "(!(-a))" },
+        { "a + b + c", "((a + b) + c)" },
+        { "a + b - c", "((a + b) - c)" },
+        { "a * b * c", "((a * b) * c)" },
+        { "a * b / c", "((a * b) / c)" },
+        { "a + b / c", "(a + (b / c))" },
+        { "a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)" },
+        { "3 + 4; -5 * 5", "(3 + 4)((-5) * 5)" },
+        { "5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))" },
+        { "5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))" },
+        { "3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))" },
+        { "1 + (2 + 3) + 4", "((1 + (2 + 3)) + 4)" },
+        { "(5 + 5) * 2", "((5 + 5) * 2)" },
+        { "2 / (5 + 5)", "(2 / (5 + 5))" },
+        { "-(5 + 5)", "(-(5 + 5))" },
+        { "!(true == true)", "(!(true == true))" }
+    };
+
+    for (const auto& tt : tests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = parser::New(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_GT(program->Statements.size(), 0) << "program.Statements does not contain statements";
+        auto result = program->String();
+        ASSERT_EQ(result, tt.expected) << "Expected: " << tt.expected << ", but got: " << result;
+    }
+}
+
+TEST(ParserTests, TestBoolean) {
+    struct BooleanTest {
+        std::string input;
+        std::string expected;
+    };
+
+    std::vector<BooleanTest> tests {
+        { "false;", "false" },
+        { "true;", "true" }
+    };
+
+    for (const auto& tt : tests) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = parser::New(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_EQ(program->Statements.size(), 1) << "Program has incorrect number of statements.";
+
+        auto stmt = dynamic_cast<ast::ExpressionStatement*>(program->Statements[0].get());
+        ASSERT_NE(stmt, nullptr) << "First statement is not an ExpressionStatement.";
+
+        auto bool_expr = dynamic_cast<ast::Boolean*>(stmt->Expression.get());
+        ASSERT_NE(bool_expr, nullptr) << "Expression is not a Boolean";
+        EXPECT_EQ(bool_expr->String(), tt.expected) << "Boolean value is incorrect.";
+        EXPECT_EQ(bool_expr->TokenLiteral(), tt.expected) << "Boolean TokenLiteral is incorrect.";
+    }
+
+    std::vector<BooleanTest> tests2 {
+        { "3 > 5 == false", "((3 > 5) == false)" },
+        { "3 < 5 == true", "((3 < 5) == true)" }
+    };
+
+    for (const auto& tt : tests2) {
+        auto lexer = std::make_unique<lexer::Lexer>(tt.input);
+        auto parser = parser::New(std::move(lexer));
+
+        auto program = parser->ParseProgram();
+
+        checkParserErrors(*parser);
+        ASSERT_GT(program->Statements.size(), 0) << "program.Statements does not contain statements";
+        auto result = program->String();
+        ASSERT_EQ(result, tt.expected) << "Expected: " << tt.expected << ", but got: " << result;
+    }
+}

47d76c3b0944e9ee2550c15ca2d0f0e58cbe0e33
Travis Harper <traviscreates@proton.me>
2024-12-31 13:37:54 -0800

    added evaluation to start program

--- repl/repl.cxx
+++ repl/repl.cxx
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <memory>
+#include <ostream>
 #include <string>
 #include <vector>
 #include "repl.h"
+#include "evaluator.h"
 #include "lexer.h"
 #include "parser.h"
 
@@ -11,9 +13,9 @@ namespace repl {
 const std::string MONKEY_ASCII_ART = R"(
        .-"-.            .-"-.          .-"-.
      _/_-.-_\_        _/.-.-.\_      _/.-.-.\_
-    / __} {__ \      /|( o o )|\    ( ( o o ) )
+    / __} {__ \      /|( > < )|\    ( ( 0 o ) )
    / //  "  \\ \    | //  "  \\ |    |/  "  \|
-  / / \'---'/ \ \  / / \'---'/ \ \    \'/^\'/
+  / / \'==='/ \ \  / / \'---'/ \ \    \'/^\'/
   \ \_/`"""`\_/ /  \ \_/`"""`\_/ /    /`\ /`\
    \           /    \           /    /  /|\  \
 )";
@@ -45,7 +47,10 @@ void Start(std::istream& in, std::ostream& out) {
             continue;
         }
 
-        out << program->String() << std::endl;
+        auto evaluated = evaluator::Eval(*program);
+        if (evaluated != nullptr) {
+            out << evaluated->Inspect() << std::endl;
+        }
     }
 }
 
